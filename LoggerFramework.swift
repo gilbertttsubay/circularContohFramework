a7449fa697c87f4917bae90676862aa0a91fb66b
Gilbert Subay <gilbertttsubay@gmail.com>
2021-10-31 16:25:15 +0700

    add file framework

---
+++ LoggerFramework.swift
@@ -0,0 +1,20 @@
+//
+//  LoggerFramework.swift
+//  circularContohFramework
+//
+//  Created by Gilbert on 31/10/21.
+//
+
+import Foundation
+import UIKit
+
+public class LoggerFramework {
+    
+    public init() {
+        
+    }
+    
+    public func printLogger(messages: String){
+        print("Ini semua karena \(messages)")
+    }
+}
